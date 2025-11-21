3a104ab915f996e7bce0fb43ac86893ee148e6b2
Gopmyc <hoyaux.gaspard@gmail.com>
2025-11-21 22:06:13 +0100

    chore(commit/srcs/main.c): 🔧 - Added internal tooling (main.c)

---
+++ srcs/main.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "auto_commit.h"
+#include "git_utils.h"
+#include "commit_builder.h"
+#include "utf8.h"
+
+static t_commit_type eGitCharToType(char c)
+{
+	switch (c)
+	{
+		case 'A': return COMMIT_ADD;
+		case '?': return COMMIT_ADD;
+		case 'M': return COMMIT_MODIFY;
+		case 'D': return COMMIT_DELETE;
+		case 'R': return COMMIT_RENAME;
+		default: return COMMIT_UNKNOWN;
+	}
+}
+
+int main(int argc, char **argv)
+{
+	t_git_file *pArrFiles;
+	size_t iFileCount;
+	int bSafeMode = 0;
+
+	if (argc > 1 && strcmp(argv[1], "--safe") == 0)
+		bSafeMode = 1;
+
+	vInitUtf8();
+	pArrFiles = arrGetGitFiles(&iFileCount);
+	if (!pArrFiles || iFileCount == 0)
+	{
+		printf("[INFO] Aucun fichier modifié détecté.\n");
+		return 0;
+	}
+
+	for (size_t i = 0; i < iFileCount; i++)
+	{
+		t_commit_type eType = eGitCharToType(pArrFiles[i].cStatus);
+		char *sMsg = strBuildCommitMessage(pArrFiles[i].sPath, eType);
+		if (!sMsg)
+			continue;
+
+		if (bSafeMode)
+		{
+			printf("[SAFE] %s\n", sMsg);
+			free(sMsg);
+			continue;
+		}
+
+		char sTmpName[256];
+		snprintf(sTmpName, sizeof(sTmpName), ".commitmsg_%lu_%zu.tmp", (unsigned long)time(NULL), i);
+
+		FILE *pTmp = fopen(sTmpName, "wb");
+		if (!pTmp)
+		{
+			free(sMsg);
+			continue;
+		}
+
+		fwrite(sMsg, 1, strlen(sMsg), pTmp);
+		fclose(pTmp);
+
+		char sCmd[2048];
+
+		switch (eType)
+		{
+			case COMMIT_ADD:
+			case COMMIT_MODIFY:
+			case COMMIT_RENAME:
+				snprintf(sCmd, sizeof(sCmd),
+					"git add \"%s\" && git commit -F \"%s\" -- \"%s\"",
+					pArrFiles[i].sPath, sTmpName, pArrFiles[i].sPath);
+				break;
+			case COMMIT_DELETE:
+				snprintf(sCmd, sizeof(sCmd),
+					"git rm \"%s\" && git commit -F \"%s\" -- \"%s\"",
+					pArrFiles[i].sPath, sTmpName, pArrFiles[i].sPath);
+				break;
+			default:
+				snprintf(sCmd, sizeof(sCmd), "echo \"[UNKNOWN] %s\"", sMsg);
+				break;
+		}
+
+		iRunGitCommand(sCmd);
+		remove(sTmpName);
+		free(sMsg);
+	}
+
+	vcFreeGitFiles(pArrFiles, iFileCount);
+	return 0;
+}

5e08f46cb177997cfed1880760c5b1b980f256b8
Gopmyc <hoyaux.gaspard@gmail.com>
2025-11-21 23:11:23 +0100

    chore(commit/srcs/main.c): 🔧 - Updated internal tooling (main.c)

--- srcs/main.c
+++ srcs/main.c
@@ -18,7 +18,7 @@ static t_commit_type eGitCharToType(char c)
 		case 'M': return COMMIT_MODIFY;
 		case 'D': return COMMIT_DELETE;
 		case 'R': return COMMIT_RENAME;
-		default: return COMMIT_UNKNOWN;
+		default:  return COMMIT_UNKNOWN;
 	}
 }
 
@@ -27,9 +27,7 @@ int main(int argc, char **argv)
 	t_git_file *arrFiles;
 	size_t iFileCount;
 	int bSafeMode = 0;
-	int bVersionIncrement = 1;
 	t_version ver;
-	char sVerStr[64];
 
 	if (argc > 1 && strcmp(argv[1], "--safe") == 0)
 		bSafeMode = 1;
@@ -49,56 +47,66 @@ int main(int argc, char **argv)
 		t_commit_type eType = eGitCharToType(arrFiles[i].cStatus);
 		int exists = (access(arrFiles[i].sPath, F_OK) == 0);
 
-		if (!bSafeMode && bVersionIncrement)
-			vIncrementVersion(&ver, 1);
-
-		vUpdateCommitHash(&ver);
-		bSaveVersion(&ver);
-
 		char *sMsg = strBuildCommitMessage(arrFiles[i].sPath, eType);
 		if (!sMsg) continue;
 
-		if (!bSafeMode)
-		{
-			char sTmpName[256];
-			snprintf(sTmpName, sizeof(sTmpName), ".commitmsg_%lu_%zu.tmp", (unsigned long)time(NULL), i);
-			FILE *pTmp = fopen(sTmpName, "wb");
-			if (pTmp)
-			{
-				fwrite(sMsg, 1, strlen(sMsg), pTmp);
-				fclose(pTmp);
+		vIncrementVersion(&ver, 1);
+		bSaveVersion(&ver);
 
-				char sCmd[2048];
-				switch (eType)
-				{
-					case COMMIT_ADD:
-					case COMMIT_MODIFY:
-					case COMMIT_RENAME:
-						snprintf(sCmd, sizeof(sCmd),
-							"git add \"%s\" \"%s\" && git commit -F \"%s\" -- \"%s\"",
-							arrFiles[i].sPath, "VERSION", sTmpName, arrFiles[i].sPath);
-						break;
-					case COMMIT_DELETE:
-						snprintf(sCmd, sizeof(sCmd),
-							"git rm \"%s\" && git commit -F \"%s\" -- \"%s\"",
-							arrFiles[i].sPath, sTmpName, arrFiles[i].sPath);
-						break;
-					default:
-						snprintf(sCmd, sizeof(sCmd), "echo \"[UNKNOWN] %s\"", sMsg);
-						break;
-				}
-				iRunGitCommand(sCmd);
-				remove(sTmpName);
-			}
-		}
-		else
+		char sVersionStr[32];
+		snprintf(sVersionStr, sizeof(sVersionStr), "%d.%d.%d", ver.major, ver.minor, ver.patch);
+
+		if (bSafeMode)
 		{
 			printf("[SAFE DEBUG] %zu | status='%c' | exists=%d | path='%s'\n",
 				i, arrFiles[i].cStatus, exists, arrFiles[i].sPath);
 			printf("[SAFE DEBUG] commit message: %s\n", sMsg);
-			vGetVersionString(&ver, sVerStr, sizeof(sVerStr));
-			printf("[SAFE DEBUG] new version: %s\n", sVerStr);
+			printf("[SAFE DEBUG] new version: %s\n", sVersionStr);
+			free(sMsg);
+			continue;
 		}
+
+		if (eType == COMMIT_DELETE && !exists)
+		{
+			printf("[SKIP] Deleted file not on disk: %s\n", arrFiles[i].sPath);
+			free(sMsg);
+			continue;
+		}
+
+		char sTmpName[256];
+		snprintf(sTmpName, sizeof(sTmpName), ".commitmsg_%lu_%zu.tmp", (unsigned long)time(NULL), i);
+
+		FILE *pTmp = fopen(sTmpName, "wb");
+		if (!pTmp)
+		{
+			free(sMsg);
+			continue;
+		}
+		fwrite(sMsg, 1, strlen(sMsg), pTmp);
+		fclose(pTmp);
+
+		char sCmd[2048];
+		switch (eType)
+		{
+			case COMMIT_ADD:
+			case COMMIT_MODIFY:
+			case COMMIT_RENAME:
+				snprintf(sCmd, sizeof(sCmd),
+					"git add \"%s\" VERSION && git commit -F \"%s\" -- \"%s\" VERSION",
+					arrFiles[i].sPath, sTmpName, arrFiles[i].sPath);
+				break;
+			case COMMIT_DELETE:
+				snprintf(sCmd, sizeof(sCmd),
+					"git rm \"%s\" && git add VERSION && git commit -F \"%s\" -- \"%s\" VERSION",
+					arrFiles[i].sPath, sTmpName, arrFiles[i].sPath);
+				break;
+			default:
+				snprintf(sCmd, sizeof(sCmd), "echo \"[UNKNOWN] %s\"", sMsg);
+				break;
+		}
+
+		iRunGitCommand(sCmd);
+		remove(sTmpName);
 		free(sMsg);
 	}
 

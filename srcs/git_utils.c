a990fc14a0e89c99dcacc53a4398e5b95f7214a9
Gopmyc <hoyaux.gaspard@gmail.com>
2025-11-21 22:28:21 +0100

    chore(commit/srcs/git_utils.c): 🔧 - Updated internal tooling (git_utils.c)

--- srcs/git_utils.c
+++ srcs/git_utils.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include "git_utils.h"
 
 #ifdef _WIN32
@@ -11,71 +12,11 @@
 # define PCLOSE pclose
 #endif
 
-static t_git_file *vcAppendFile(t_git_file *arr, size_t *piLen, size_t *piCap, const char *sPath, char cStatus)
-{
-	char *sDup = malloc(strlen(sPath) + 1);
-	if (!sDup)
-		return arr;
-	strcpy(sDup, sPath);
-
-	if (*piLen + 1 >= *piCap)
-	{
-		*piCap *= 2;
-		t_git_file *pNew = realloc(arr, sizeof(t_git_file) * (*piCap));
-		if (!pNew)
-		{
-			free(sDup);
-			return arr;
-		}
-		arr = pNew;
-	}
-
-	arr[*piLen].sPath = sDup;
-	arr[*piLen].cStatus = cStatus;
-	(*piLen)++;
-	return arr;
-}
-
-static void vcReadGitDiff(const char *sCmd, t_git_file **arr, size_t *piLen, size_t *piCap, int bRename)
-{
-	FILE *p = POPEN(sCmd, "r");
-	if (!p)
-		return;
-
-	char buf[1024];
-	while (fgets(buf, sizeof(buf), p))
-	{
-		size_t i = strlen(buf);
-		while (i > 0 && (buf[i-1]=='\n'||buf[i-1]=='\r')) buf[--i]='\0';
-		if (i < 2) continue;
-
-		char cStatus = buf[0];
-		char *sPath = buf + 1;
-		while (*sPath=='\t'||*sPath==' ') sPath++;
-
-		if (bRename && cStatus == 'R')
-		{
-			// Pour les fichiers renommés, on prend le chemin de destination (2ème colonne)
-			char *sDest = strchr(sPath, '\t');
-			if (!sDest)
-				sDest = strchr(sPath, ' ');
-			if (sDest)
-			{
-				while (*sDest=='\t'||*sDest==' ') sDest++;
-				sPath = sDest;
-			}
-		}
-
-		*arr = vcAppendFile(*arr, piLen, piCap, sPath, cStatus);
-	}
-	PCLOSE(p);
-}
-
 t_git_file *arrGetGitFiles(size_t *piCount)
 {
 	FILE *p;
 	char buf[4096];
-	size_t iCap = 32;
+	size_t iCap = 16;
 	size_t iLen = 0;
 	t_git_file *arr;
 
@@ -94,64 +35,68 @@ t_git_file *arrGetGitFiles(size_t *piCount)
 
 	while (fgets(buf, sizeof(buf), p))
 	{
-		size_t i = strlen(buf);
-		while (i > 0 && (buf[i - 1] == '\n' || buf[i - 1] == '\r'))
-			buf[--i] = '\0';
-		if (i == 0)
+		size_t len = strlen(buf);
+		while (len > 0 && (buf[len-1] == '\n' || buf[len-1] == '\r'))
+			buf[--len] = '\0';
+		if (len == 0)
 			continue;
 
-		char cStatus = buf[0];
+		char cStatus;
 		char *sPath1 = NULL;
 		char *sPath2 = NULL;
 
-		if (cStatus == 'R') // renamed
+		if (buf[0] == 'R')
 		{
-			// Format: R<score>\told_path\tnew_path
+			cStatus = 'R';
 			char *tab1 = strchr(buf, '\t');
 			if (!tab1) continue;
-			char *tab2 = strchr(tab1 + 1, '\t');
-			if (!tab2) continue;
+			*tab1 = 0;
+			sPath1 = tab1 + 1;
 
-			*tab2 = '\0';
-			sPath1 = tab1 + 1; // old
-			sPath2 = tab2 + 1; // new
+			char *tab2 = strchr(sPath1, '\t');
+			if (!tab2) continue;
+			*tab2 = 0;
+			sPath2 = tab2 + 1;
 		}
-		else if (cStatus == '?' || cStatus == 'M' || cStatus == 'A' || cStatus == 'D')
+		else if (buf[0] == 'A' || buf[0] == 'M' || buf[0] == 'D')
 		{
-			// tab-separated after status
-			char *tab = buf + 1;
-			while (*tab == '\t' || *tab == ' ')
-				tab++;
-			sPath1 = tab;
+			cStatus = buf[0];
+			sPath1 = buf + 1;
+			while (*sPath1 == '\t' || *sPath1 == ' ')
+				sPath1++;
 		}
 		else
 		{
-			continue;
+			cStatus = '?';
+			sPath1 = buf;
 		}
 
-		if (iLen + 1 >= iCap)
+		if (iLen + (sPath2 ? 2 : 1) >= iCap)
 		{
 			iCap *= 2;
-			t_git_file *pNew = realloc(arr, sizeof(t_git_file) * iCap);
-			if (!pNew) break;
-			arr = pNew;
+			t_git_file *tmp = realloc(arr, sizeof(t_git_file) * iCap);
+			if (!tmp) break;
+			arr = tmp;
 		}
 
-		if (cStatus == 'R')
-		{
-			arr[iLen].sPath = strdup(sPath2);
-			arr[iLen].cStatus = cStatus;
-		}
-		else
-		{
-			arr[iLen].sPath = strdup(sPath1);
-			arr[iLen].cStatus = cStatus;
-		}
-		if (!arr[iLen].sPath) continue;
+		char *dup1 = strdup(sPath1);
+		if (!dup1) continue;
+		arr[iLen].cStatus = cStatus;
+		arr[iLen].sPath = dup1;
 		iLen++;
+
+		if (sPath2)
+		{
+			char *dup2 = strdup(sPath2);
+			if (!dup2) continue;
+			arr[iLen].cStatus = cStatus;
+			arr[iLen].sPath = dup2;
+			iLen++;
+		}
 	}
 
 	PCLOSE(p);
+
 	if (iLen == 0)
 	{
 		free(arr);
@@ -164,8 +109,7 @@ t_git_file *arrGetGitFiles(size_t *piCount)
 
 int iRunGitCommand(const char *sCmd)
 {
-	if (!sCmd)
-		return 1;
+	if (!sCmd) return 1;
 	return system(sCmd);
 }
 

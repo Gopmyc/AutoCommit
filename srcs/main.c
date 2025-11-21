5ace2dd742746307037ebf3aabfbabb9098f2784
Gopmyc <hoyaux.gaspard@gmail.com>
2025-11-21 22:21:54 +0100

    chore(commit/srcs/main.c): 🔧 - Updated internal tooling (main.c)

--- srcs/main.c
+++ srcs/main.c
@@ -31,7 +31,6 @@ int main(int argc, char **argv)
 		bSafeMode = 1;
 
 	vInitUtf8();
-
 	pArrFiles = arrGetGitFiles(&iFileCount);
 	if (!pArrFiles || iFileCount == 0)
 	{
@@ -53,6 +52,13 @@ int main(int argc, char **argv)
 			continue;
 		}
 
+		if ((eType == COMMIT_ADD || eType == COMMIT_MODIFY || eType == COMMIT_RENAME) && access(pArrFiles[i].sPath, F_OK) != 0)
+		{
+			printf("[SKIP] %s (file missing)\n", pArrFiles[i].sPath);
+			free(sMsg);
+			continue;
+		}
+
 		char sTmpName[256];
 		snprintf(sTmpName, sizeof(sTmpName), ".commitmsg_%lu_%zu.tmp", (unsigned long)time(NULL), i);
 
@@ -62,59 +68,33 @@ int main(int argc, char **argv)
 			free(sMsg);
 			continue;
 		}
-
 		fwrite(sMsg, 1, strlen(sMsg), pTmp);
 		fclose(pTmp);
 
-		char sCmd[4096];
+		char sCmd[2048];
+
 		switch (eType)
 		{
 			case COMMIT_ADD:
 			case COMMIT_MODIFY:
-				if (access(pArrFiles[i].sPath, F_OK) == 0)
+				snprintf(sCmd, sizeof(sCmd),
+					"git add \"%s\" && git commit -F \"%s\" -- \"%s\"",
+					pArrFiles[i].sPath, sTmpName, pArrFiles[i].sPath);
+				break;
+			case COMMIT_DELETE:
+				snprintf(sCmd, sizeof(sCmd),
+					"git rm \"%s\" && git commit -F \"%s\" -- \"%s\"",
+					pArrFiles[i].sPath, sTmpName, pArrFiles[i].sPath);
+				break;
+			case COMMIT_RENAME:
+				{
+					char sOldPath[1024];
+					snprintf(sOldPath, sizeof(sOldPath), "%s", pArrFiles[i].sPath);
 					snprintf(sCmd, sizeof(sCmd),
 						"git add \"%s\" && git commit -F \"%s\" -- \"%s\"",
 						pArrFiles[i].sPath, sTmpName, pArrFiles[i].sPath);
-				else
-					snprintf(sCmd, sizeof(sCmd),
-						"echo \"[SKIP] %s\"",
-						pArrFiles[i].sPath);
-				break;
-
-			case COMMIT_DELETE:
-				if (access(pArrFiles[i].sPath, F_OK) == 0)
-					snprintf(sCmd, sizeof(sCmd),
-						"git rm \"%s\" && git commit -F \"%s\" -- \"%s\"",
-						pArrFiles[i].sPath, sTmpName, pArrFiles[i].sPath);
-				else
-					snprintf(sCmd, sizeof(sCmd),
-						"git rm --cached \"%s\" && git commit -F \"%s\" -- \"%s\"",
-						pArrFiles[i].sPath, sTmpName, pArrFiles[i].sPath);
-				break;
-
-			case COMMIT_RENAME:
-			{
-				char *sSpace = strchr(pArrFiles[i].sPath, ' ');
-				if (sSpace)
-				{
-					*sSpace = '\0';
-					const char *sOld = pArrFiles[i].sPath;
-					const char *sNew = sSpace + 1;
-
-					if (access(sNew, F_OK) == 0)
-						snprintf(sCmd, sizeof(sCmd),
-							"git add \"%s\" && git rm --cached \"%s\" && git commit -F \"%s\" -- \"%s\"",
-							sNew, sOld, sTmpName, sNew);
-					else
-						snprintf(sCmd, sizeof(sCmd),
-							"echo \"[SKIP RENAME] %s -> %s\"", sOld, sNew);
 				}
-				else
-					snprintf(sCmd, sizeof(sCmd),
-						"echo \"[INVALID R FORMAT] %s\"", pArrFiles[i].sPath);
 				break;
-			}
-
 			default:
 				snprintf(sCmd, sizeof(sCmd), "echo \"[UNKNOWN] %s\"", sMsg);
 				break;

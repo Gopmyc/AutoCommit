5fb2839cbec2dec39b7843f5722b88c176bbeb8f
Gopmyc <hoyaux.gaspard@gmail.com>
2025-11-21 23:03:41 +0100

    chore(commit/srcs/main.c): 🔧 - Updated internal tooling (main.c)

--- srcs/main.c
+++ srcs/main.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <unistd.h>
 #include "auto_commit.h"
 #include "git_utils.h"
 #include "commit_builder.h"
 #include "utf8.h"
+#include "version.h"
 
 static t_commit_type eGitCharToType(char c)
 {
@@ -16,90 +18,90 @@ static t_commit_type eGitCharToType(char c)
 		case 'M': return COMMIT_MODIFY;
 		case 'D': return COMMIT_DELETE;
 		case 'R': return COMMIT_RENAME;
-		default:  return COMMIT_UNKNOWN;
+		default: return COMMIT_UNKNOWN;
 	}
-}
-
-static void vPushTagPatch(void)
-{
-	char last_tag[128] = {0};
-	char new_tag[128];
-	int major=0, minor=0, patch=0;
-
-	FILE *fp = popen("git tag --list \"v*\" --sort=-creatordate", "r");
-	if (fp && fgets(last_tag, sizeof(last_tag), fp))
-	{
-		size_t len = strlen(last_tag);
-		if (len > 0 && (last_tag[len-1]=='\n'||last_tag[len-1]=='\r'))
-			last_tag[len-1] = 0;
-		sscanf(last_tag, "v%d.%d.%d", &major, &minor, &patch);
-	}
-	if (fp) pclose(fp);
-
-	patch++;
-	snprintf(new_tag, sizeof(new_tag), "v%d.%d.%d", major, minor, patch);
-
-	char cmd[256];
-	snprintf(cmd, sizeof(cmd), "git tag -a %s -m \"Release %s\"", new_tag, new_tag);
-	system(cmd);
-	snprintf(cmd, sizeof(cmd), "git push origin %s", new_tag);
-	system(cmd);
-	printf("[TAG] %s\n", new_tag);
 }
 
 int main(int argc, char **argv)
 {
-	t_git_file *pArrFiles;
+	t_git_file *arrFiles;
 	size_t iFileCount;
 	int bSafeMode = 0;
+	int bVersionIncrement = 1;
+	t_version ver;
+	char sVerStr[64];
 
 	if (argc > 1 && strcmp(argv[1], "--safe") == 0)
 		bSafeMode = 1;
 
 	vInitUtf8();
-	pArrFiles = arrGetGitFiles(&iFileCount);
-	if (!pArrFiles || iFileCount == 0)
+	bLoadVersion(&ver);
+
+	arrFiles = arrGetGitFiles(&iFileCount);
+	if (!arrFiles || iFileCount == 0)
 	{
 		printf("[INFO] Aucun fichier modifié détecté.\n");
 		return 0;
 	}
 
-	for (size_t i=0; i<iFileCount; i++)
+	for (size_t i = 0; i < iFileCount; i++)
 	{
-		t_commit_type eType = eGitCharToType(pArrFiles[i].cStatus);
-		char *sMsg = strBuildCommitMessage(pArrFiles[i].sPath, eType);
+		t_commit_type eType = eGitCharToType(arrFiles[i].cStatus);
+		int exists = (access(arrFiles[i].sPath, F_OK) == 0);
+
+		if (!bSafeMode && bVersionIncrement)
+			vIncrementVersion(&ver, 1);
+
+		vUpdateCommitHash(&ver);
+		bSaveVersion(&ver);
+
+		char *sMsg = strBuildCommitMessage(arrFiles[i].sPath, eType);
 		if (!sMsg) continue;
 
-		if (bSafeMode)
+		if (!bSafeMode)
 		{
-			printf("[SAFE] %s\n", sMsg);
-			free(sMsg);
-			continue;
-		}
+			char sTmpName[256];
+			snprintf(sTmpName, sizeof(sTmpName), ".commitmsg_%lu_%zu.tmp", (unsigned long)time(NULL), i);
+			FILE *pTmp = fopen(sTmpName, "wb");
+			if (pTmp)
+			{
+				fwrite(sMsg, 1, strlen(sMsg), pTmp);
+				fclose(pTmp);
 
-		char cmd[2048];
-		switch (eType)
+				char sCmd[2048];
+				switch (eType)
+				{
+					case COMMIT_ADD:
+					case COMMIT_MODIFY:
+					case COMMIT_RENAME:
+						snprintf(sCmd, sizeof(sCmd),
+							"git add \"%s\" \"%s\" && git commit -F \"%s\" -- \"%s\"",
+							arrFiles[i].sPath, "VERSION", sTmpName, arrFiles[i].sPath);
+						break;
+					case COMMIT_DELETE:
+						snprintf(sCmd, sizeof(sCmd),
+							"git rm \"%s\" && git commit -F \"%s\" -- \"%s\"",
+							arrFiles[i].sPath, sTmpName, arrFiles[i].sPath);
+						break;
+					default:
+						snprintf(sCmd, sizeof(sCmd), "echo \"[UNKNOWN] %s\"", sMsg);
+						break;
+				}
+				iRunGitCommand(sCmd);
+				remove(sTmpName);
+			}
+		}
+		else
 		{
-			case COMMIT_ADD:
-			case COMMIT_MODIFY:
-			case COMMIT_RENAME:
-				snprintf(cmd, sizeof(cmd), "git add \"%s\" && git commit -m \"%s\" -- \"%s\"",
-					pArrFiles[i].sPath, sMsg, pArrFiles[i].sPath);
-				break;
-			case COMMIT_DELETE:
-				snprintf(cmd, sizeof(cmd), "git rm \"%s\" && git commit -m \"%s\" -- \"%s\"",
-					pArrFiles[i].sPath, sMsg, pArrFiles[i].sPath);
-				break;
-			default:
-				snprintf(cmd, sizeof(cmd), "echo \"[UNKNOWN] %s\"", sMsg);
-				break;
+			printf("[SAFE DEBUG] %zu | status='%c' | exists=%d | path='%s'\n",
+				i, arrFiles[i].cStatus, exists, arrFiles[i].sPath);
+			printf("[SAFE DEBUG] commit message: %s\n", sMsg);
+			vGetVersionString(&ver, sVerStr, sizeof(sVerStr));
+			printf("[SAFE DEBUG] new version: %s\n", sVerStr);
 		}
-
-		iRunGitCommand(cmd);
-		vPushTagPatch();
 		free(sMsg);
 	}
 
-	vcFreeGitFiles(pArrFiles, iFileCount);
+	vcFreeGitFiles(arrFiles, iFileCount);
 	return 0;
 }
